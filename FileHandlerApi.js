95d3ae122547af4245bfc1a095578713e3d41499
Pawan Sasanka <pawan@duosoftware.com>
2017-09-04 14:36:19 +0530

    mongo download fixed

--- FileHandlerApi.js
+++ FileHandlerApi.js
@@ -201,7 +201,6 @@ function MongoFileDownloader(UUID,isEncryptedFile,method,reqId,res) {
         }
         else
         {
-            try {
                 var bucket = new mongodb.GridFSBucket(db, {
                     chunkSizeBytes: 1024
                 });
@@ -246,13 +245,7 @@ function MongoFileDownloader(UUID,isEncryptedFile,method,reqId,res) {
 
                     });
                 }
-            } catch (e) {
-                console.log(e);
-                logger.debug('[DVP-FIleService.MongoFileDownloader] - [%s] - Exception in operation  ', reqId);
-                res.status(400);
-                db.close();
-                res.end();
-            }
+
 
 
         }
