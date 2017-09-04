a1cc339d787c31b2eba09787dde7627b6eb39037
Pawan Sasanka <pawan@duosoftware.com>
2017-09-04 15:37:15 +0530

    file category search updated

--- DeveloperFileUpoladManager.js
+++ DeveloperFileUpoladManager.js
@@ -817,7 +817,7 @@ function DeveloperUploadFiles(fileObj,callback)
         }
 
 
-        searchFileCategory(fileObj.Category,fileObj.reqId,function (resCat,errCat) {
+        searchFileCategory(fileObj.Category,fileObj.reqId,function (errCat,resCat) {
 
             if(errCat)
             {
