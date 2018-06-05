f35f4c03b991ab462e56671efe672ba39b6dab3e
sine19 <sineth17@gmail.com>
2018-06-05 13:31:47 +0530

    clear all values
    remove all default.js and conf.js values

--- config/default.js
+++ config/default.js
@@ -1,21 +1,21 @@
 module.exports = {
   "DB": {
     "Type":"postgres",
-    "User":"duo",
-    "Password":"DuoS123",
+    "User":"",
+    "Password":"",
     "Port":5432,
-    "Host":"104.236.231.11",
-    "Database":"duo"
+    "Host":"",
+    "Database":""
   },
   "Redis":
   {
     "mode":"sentinel",//instance, cluster, sentinel
-    "ip": "45.55.142.207",
+    "ip": "",
     "port": 6389,
-    "user": "duo",
-    "password": "DuoS123",
+    "user": "",
+    "password": "",
     "sentinels":{
-      "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
+      "hosts": "",
       "port":16389,
       "name":"redis-cluster"
     }
@@ -26,13 +26,13 @@ module.exports = {
   "Security":
   {
 
-    "ip" : "45.55.142.207",
+    "ip" : "",
     "port": 6389,
-    "user": "duo",
-    "password": "DuoS123",
+    "user": "",
+    "password": "",
     "mode":"sentinel",//instance, cluster, sentinel
     "sentinels":{
-      "hosts": "138.197.90.92,45.55.205.92,138.197.90.92",
+      "hosts": "",
       "port":16389,
       "name":"redis-cluster"
     }
@@ -53,11 +53,11 @@ module.exports = {
 
   "Mongo":
   {
-    "ip":"104.236.231.11",
+    "ip":"",
     "port":"27017",
-    "dbname":"dvpdb",
-    "password":"DuoS123",
-    "user":"duo"
+    "dbname":"",
+    "password":"",
+    "user":""
   },
 
   "Couch":
@@ -65,8 +65,8 @@ module.exports = {
     "ip":"127.0.0.1",
     "port":"",
     "bucket":"default",
-    "user":"duo",
-    "password":"DuoS123"
+    "user":"",
+    "password":""
 
   },
   "Crypto":
@@ -78,8 +78,8 @@ module.exports = {
   "BasePath":"./upload",
 
     "Services" : {
-        "accessToken":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdWtpdGhhIiwianRpIjoiYWEzOGRmZWYtNDFhOC00MWUyLTgwMzktOTJjZTY0YjM4ZDFmIiwic3ViIjoiNTZhOWU3NTlmYjA3MTkwN2EwMDAwMDAxMjVkOWU4MGI1YzdjNGY5ODQ2NmY5MjExNzk2ZWJmNDMiLCJleHAiOjE5MDIzODExMTgsInRlbmFudCI6LTEsImNvbXBhbnkiOi0xLCJzY29wZSI6W3sicmVzb3VyY2UiOiJhbGwiLCJhY3Rpb25zIjoiYWxsIn1dLCJpYXQiOjE0NzAzODExMTh9.Gmlu00Uj66Fzts-w6qEwNUz46XYGzE8wHUhAJOFtiRo",
-        "userServiceHost": "userservice.app.veery.cloud",
+        "accessToken":"",
+        "userServiceHost": "",
         "userServiceServicePort": "3638",
         "userServiceVersion": "1.0.0.0"
     },
